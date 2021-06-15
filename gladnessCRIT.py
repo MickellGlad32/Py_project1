12e7d23d3a176038c78b702a6a75b39b21d53b55
Mickell Gladness <mickellgladness@gmail.com>
2021-06-15 10:09:09 -0400

    Initial commit

--- gladnessCRIT.py
+++ gladnessCRIT.py
@@ -7,27 +7,103 @@
 
 import random
 
-board = []
-players = []
+class Board:
+    def __init__(self):
+        self.places = []
+        count = 1
+        self.players = []
+        while len(self.places) < 20:
+            self.places.append(Place(count))
+            count += 1
 
-class Place:
-    def __init__(self,cyclist_name):
-        self.cyclist_name = cyclist_name
-        self.position = len(board)
+    def winner(self):
+        if self.places[-1].player != None:
+            return self.places[-1].player
+        else:
+            return False
 
     def show(self):
-        on = []
-        for p in players:
-            if p.position == self.position:
-                on.append(p.cyclist_name)
-        print(self.cyclist_name + str(on), end = "\t")
+        print('Current cyclist position. ')
+        for player in self.players:
+            player_on_board = False
+
+            for place in self.places:
+                if place.player == player:
+                    print(f"{place.player.name} is on position {place.position}")
+
+                    player_on_board = True
+
+            if not player_on_board:
+                print(f"{player.name} is not on the board yet.")
+            
+        
+
+    def add_player(self, player):
+        self.players.append(player)
+        
+
+    def move_player(self, player, distance):
+        current_position = 0
+        index = 0
+        for place in self.places:
+            if place.player == player:
+                current_position = index
+
+            index += 1
+        new_position = current_position + distance
+        if new_position >= len(self.places) -1:
+            new_position = len(self.places) -1
+        if self.places[new_position].player != None:
+            pass
+        else:
+            self.places[current_position].remove_player()
+            self.places[new_position].add_player(player)
+
+
+class Place:
+    def __init__(self, position=0):
+        self.position = position
+        self.player = None
+
+    def add_player(self, player):
+        self.player = player
+        
+
+    def remove_player(self):
+        self.player = None
 
 class Player:
-    def __init__(self):
+    def __init__(self,name):
         self.position = 0
-        self.cyclist_name = "P" + str(len(players))
+        self.name = name
         self.places = []
-        players.append(self)
-
         
+        print(f"Welcome to Crit Racing {name}")
+
+    def __str__(self):
+        return self.name
+        
+
+    def turn(self, player):
+       while player is True:
+        print(f"It's {player}'\s' turn to roll.")
+        dice_roll  = random.randint(1, 12)
+        print(dice_roll)
+        board.move_player(player1, dice_roll)
+        board.show()
+        roll += 1
+
+
+        print(f"It's {self.name} Turn to go")
+        print("2) Roll Dice")
+
+        choice = input(": ")
+        print(choice)
+
+        if int(choice) == 1:
+            for player in self.places:
+                print(f"You have landed on {player.name}")
+
+                
+            
 
--- main.py
+++ main.py
@@ -1,31 +1,64 @@
-import gladnessCRIT
-import pygame
+from gladnessCRIT import Player, Board
+import random
+import time
 
-# inititalize pygame
-pygame.init()
+board = Board()
 
-# creating a screen
-screen = pygame.display.set_mode([800, 600])
-# pygame.display.update()
+player1_name = input("Player1 enter your name: ")
+player1 = Player(player1_name)
 
-pygame.display.set_caption("Tour de Gladnes")
-# pygame.image.load('adrianagladness/downloads/cycling.png')
+player2_name = input("Player2 enter your name: ")
+player2 = Player(player2_name)
+
+board.add_player(player1)
+board.add_player(player2)
+board.show()
+
+# Each player rolls the dice, and the higher number gets the first chance to go.
+print("Players, roll the dice, and let's see who goes first. ")
+
+time.sleep(1)
+player1_first_roll = random.randint(1, 12)
+print(player1_first_roll)
+
+print("It would only be fair if we let the other player roll as well.")
+print("Next player your turn to roll. ")
+player2_first_roll = random.randint(1,12)
+print(player2_first_roll)
+
+time.sleep(3)
+
+if player1_first_roll > player2_first_roll:
+    print(f"{player1} let's see if you can hold on to this lead. ")
+    print(f"{player1} roll again. Don't let them catch your wheel!")
+    player1_second_roll = random.randint(1, 12)
+    board.move_player(player1, player1_second_roll)
+    board.show()
+    current_player = player2
 
 
-# Game loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-# Display screen color
-    screen.fill((0,128,128))
-# Draw a solid circle in the center of the screen
-    pygame.draw.circle(screen, (255,255, 0), (700, 100), 90)
+else:
+    print(f"{player2}, it seems that luck favors you. Let's hope that your legs are up to the challenge.")
+    print(f"{player2} roll again and set the pace. ")
+    player2_second_roll = random.randint(1, 12)
+    board.move_player(player2, player2_second_roll)
+    board.show()
+    current_player = player1
+    
+while not board.winner(): 
 
-    pygame.display.flip()
+    time.sleep(2)
 
     
+    print(f"It's {current_player}'\s' turn to roll.")
+    dice_roll  = random.randint(1, 12)
+    print(dice_roll)
+    board.move_player(current_player, dice_roll)
+    board.show()
+    if current_player == player1:
+        current_player = player2
+    else:
+        current_player = player1
 
-pygame.quit()
+print(f"{board.winner().name} is the winner!!! ")
 
